13898c1ea223afd42b8ba76b4e5f3707d4723f89
Ritika Rajput <ritika.bodex@gmail.com>
2026-07-28 13:07:11 +0530

    Fix PORT variable

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => {
     console.log(err);
   });
+  const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
